8fb90b138e8a1f9825414faab779bd08b33b4fb0
Maksim Gostev <teranys1986@gmail.com>
2023-01-12 10:41:18 +0300

    шаг 2 сделан

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ def get_movie(title):
     result = utils.search_by_name(title)
     return jsonify(result)
 
+@app.route("/movie/<year_1>/to/<year_2>")
+def get_by_year(year_1, year_2):
+    result = utils.search_by_range_of_release_years(year_1, year_2)
+    return jsonify(result)
+
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -21,7 +21,25 @@ def search_by_name(title):
                    }
     return result_dict
 
-print(search_by_name(100))
+def search_by_range_of_release_years(year_1, year_2):
+    with sqlite3.connect("netflix.db") as con:
+        cur = con.cursor()
+        sqlite_query = ("SELECT title, release_year "
+                        "FROM netflix "
+                        f"WHERE release_year BETWEEN {year_1} AND {year_2} "
+                        "LIMIT 100")
+        cur.execute(sqlite_query)
+        result = cur.fetchall()
+    result_list = []
+    for res in result:
+        result_dict = {
+                       "title": res[0],
+                       "release_year": res[1],
+                      }
+        result_list.append(result_dict)
+    return result_list
+
+
 
 
 
